55dec0d39ef0e7b16e2b0ea1f9f2026a53c94466
Quentin Quadrat <lecrapouille@gmail.com>
2021-08-22 23:27:02 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <map>
+#include <deque>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+#define ZOOM 1.0f
+#define RAD2DEG(r) ((r) * 57.295779513f)
+#define DEG2RAD(d) ((d) * 0.01745329251994f)
 
 struct ParkingDimension
 {
@@ -51,9 +58,9 @@ struct CarDimension
     //! \brief Rayon roue [meter]
     float wheel_radius;
     //! \brief Epaisseur roue [meter] (only used for the rendering)
-    //float wheel_width = 0.1f;
+    float wheel_width = 0.1f;
     //! \brief Limit of steering angle absolute angle [rad]
-    //float max_steering_angle;
+    float max_steering_angle;
 };
 
 class CarDimensions
@@ -97,28 +104,28 @@ struct TurningRadius
         const float p = dim.front_overhang;
 
         // Rayon de braquage [m] (turning radius)
-        R = e / sinf(steering);
+        middle_front = e / sinf(steering);
 
         // Inner radius [m]
-        Ri = sqrtf(R * R - e * e) - (w / 2.0f);
+        internal = sqrtf(middle_front * middle_front - e * e) - (w / 2.0f);
 
         // Outer radius [m]
-        Re = sqrtf((.Ri + w) * (Ri + w) + (e + p) * (e + p));
+        external = sqrtf((internal + w) * (internal + w) + (e + p) * (e + p));
 
         // Largeur balayee [m]
-        bal = Re - Ri;
+        bal = external - internal;
 
         // surlargeur [m]
         sur = bal - w;
     }
 
-    //! \brief turning radius of the virtual wheel located in the middle of
-    //! the front axle [meter].
-    float R;
+    //! \brief turning radius of the virtual wheel located in the middle of the
+    //! front axle [meter].
+    float middle_front;
     //! \brief turning radius of the internal wheel [meter].
-    float Ri;
+    float internal;
     //! \brief turning radius of the external wheel [meter].
-    float Re;
+    float external;
     //! \brief Largeur balayee [meter]
     float bal;
     //! \brief Surlargeur [meter]
@@ -143,169 +150,342 @@ class CarShape
 {
 public:
 
-    CarShape(CarDimension const& dim, sf::Vector2f const& back, float const yaw, float const steering)
-       : dim(d)
+    CarShape(CarDimension const& dim_)
+       : dim(dim_)
     {
         // Wheel offset along the Y-axis
         const float K = dim.width / 2 - dim.wheel_width / 2;
 
-        wheels[FL].offset = sf::Vector2f(dim.wheelbase, -K);
-        wheels[FR].offset = sf::Vector2f(dim.wheelbase, K);
-        wheels[RL].offset = sf::Vector2f(0.0f, -K);
-        wheels[RR].offset = sf::Vector2f(0.0f, K);
-        //wheels[FL].speed = wheels[FR].speed = NAN;
-        //wheels[RL].speed = wheels[RR].speed = NAN;
-
-        update(back, yaw, steering)
+        m_wheels[Wheel::Type::FL].offset = sf::Vector2f(dim.wheelbase, -K);
+        m_wheels[Wheel::Type::FR].offset = sf::Vector2f(dim.wheelbase, K);
+        m_wheels[Wheel::Type::RL].offset = sf::Vector2f(0.0f, -K);
+        m_wheels[Wheel::Type::RR].offset = sf::Vector2f(0.0f, K);
+        //wheels[FL].speed = m_wheels[FR].speed = NAN;
+        //wheels[RL].speed = m_wheels[RR].speed = NAN;
     }
 
-    void update(sf::Vector2f const& back, float const yaw, float const steering)
+    void set(sf::Vector2f const& position, float const heading, float const steering)
     {
-        m_heading = yaw_;
-        m_steering = steering_;
-        m_position = back;
+        m_heading = heading;
+        m_position = position;
 
-        const float COS_YAW = cosf(yaw);
-        const float SIN_YAW = sinf(yaw);
-        const float FRONT_X = back.x + wheels[FL].offset.x * COS_YAW;
-        const float FRONT_Y = back.y + wheels[FL].offset.x * SIN_YAW;
-        const float OFFSET_SIN = wheels[FL].offset.y * SIN_YAW;
-        const float OFFSET_COS = wheels[FL].offset.y * COS_YAW;
+        const float COS_YAW = cosf(heading);
+        const float SIN_YAW = sinf(heading);
+        const float FRONT_X = position.x + m_wheels[Wheel::Type::FL].offset.x * COS_YAW;
+        const float FRONT_Y = position.y + m_wheels[Wheel::Type::FL].offset.x * SIN_YAW;
+        const float OFFSET_SIN = m_wheels[Wheel::Type::FL].offset.y * SIN_YAW;
+        const float OFFSET_COS = m_wheels[Wheel::Type::FL].offset.y * COS_YAW;
 
-        wheels[FL].position.x = FRONT_X - OFFSET_SIN;
-        wheels[FL].position.y = FRONT_Y + OFFSET_COS;
-        wheels[FR].position.x = FRONT_X + OFFSET_SIN;
-        wheels[FR].position.x = FRONT_Y - OFFSET_COS;
+        m_wheels[Wheel::Type::FL].position.x = FRONT_X - OFFSET_SIN;
+        m_wheels[Wheel::Type::FL].position.y = FRONT_Y + OFFSET_COS;
+        m_wheels[Wheel::Type::FR].position.x = FRONT_X + OFFSET_SIN;
+        m_wheels[Wheel::Type::FR].position.x = FRONT_Y - OFFSET_COS;
 
-        wheels[RL].position.x = back.x + OFFSET_SIN;
-        wheels[RL].position.y = back.y - OFFSET_COS;
-        wheels[RR].position.x = back.x - OFFSET_SIN;
-        wheels[RR].position.x = back.y + OFFSET_COS;
+        m_wheels[Wheel::Type::RL].position.x = position.x + OFFSET_SIN;
+        m_wheels[Wheel::Type::RL].position.y = position.y - OFFSET_COS;
+        m_wheels[Wheel::Type::RR].position.x = position.x - OFFSET_SIN;
+        m_wheels[Wheel::Type::RR].position.x = position.y + OFFSET_COS;
 
-        wheels[FL].steering = wheels[FR].steering = steering;
-        wheels[RL].steering = wheels[RR].steering = 0.0f;
+        m_wheels[Wheel::Type::FL].steering = m_wheels[Wheel::Type::FR].steering = steering;
+        m_wheels[Wheel::Type::RL].steering = m_wheels[Wheel::Type::RR].steering = 0.0f;
     }
 
     inline std::array<Wheel, 4> const& wheels() const { return m_wheels; }
     inline Wheel const& wheel(Wheel::Type const i) const { return m_wheels[i]; }
     inline float heading() const { return m_heading; }
-    inline float steering() const { return m_steering; }
-    inline sf::Vector2f position() const { return position; }
+    inline float steering() const
+    {
+        return m_wheels[0].steering;
+    }
+
+    void steering(float const v)
+    {
+        m_wheels[Wheel::Type::FL].steering = m_wheels[Wheel::Type::FR].steering = v;
+    }
+    inline sf::Vector2f position() const { return m_position; }
 
 public:
 
-    CarDimensions const& dim;
+    CarDimension const& dim;
 
 protected:
 
     std::array<Wheel, 4> m_wheels;
-    sf::Vector2f const& m_position;
+    sf::Vector2f m_position;
     float m_heading;
-    float m_steering;
 };
 
-class Kinematic
+class CarControl
 {
 public:
 
-    Kinematic(CarShape& shape_)
-        : shape(shape_)
+    void set()
     {}
 
-    void init(sf::Vector2f const& position, float heading)
+    //----------------------------------------------------------------------
+    //! \brief The desired
+    //----------------------------------------------------------------------
+    struct References
     {
-        shape.position = position;
-        shape.heading = heading;
+        float body_speed = 0.0f;
+        float steering = 0.0f;
+    };
+
+    //----------------------------------------------------------------------
+    //! \brief
+    //----------------------------------------------------------------------
+    struct Inputs
+    {
+    };
+
+    //----------------------------------------------------------------------
+    //! \brief
+    //----------------------------------------------------------------------
+    struct Outputs
+    {
+        float body_speed = 0.0f;
+        float steering = 0.0f;
+    };
+
+    //----------------------------------------------------------------------
+    //! \brief
+    //----------------------------------------------------------------------
+    void update(float const /*dt*/ /*, sensors*/)
+    {
+        outputs.body_speed = ref.body_speed;
+        outputs.steering = ref.steering;
     }
 
-    void update(float const dt, Control const& control);
-    {
-        float const steering = control.outputs.steering;
-        float const speed = control.outputs.speed;
+    References ref;
+    Outputs outputs;
+};
 
-        float heading += dt * body_speed * tanf(steering) / shape.dim.wheelbase;
-        float position.x += dt * body_speed * cosf(heading);
-        float position.y += dt * body_speed * sinf(heading);
+class CarPhysics
+{
+public:
+
+    virtual ~CarPhysics() = default;
+    virtual void update(float const dt, CarControl const& control) = 0;
+    virtual float acceleration() const = 0;
+    virtual float speed() const = 0;
+    virtual sf::Vector2f position() const = 0;
+};
+
+class CarKinematic: public CarPhysics
+{
+public:
+
+    CarKinematic(CarShape& shape)
+        : m_shape(shape)
+    {}
+
+    void init(sf::Vector2f const& position, float const speed,
+              float const heading, float const steering)
+    {
+        m_shape.set(position, heading, steering);
+        m_speed = speed;
+    }
+
+    virtual void update(float const dt, CarControl const& control) override
+    {
+        float steering = control.outputs.steering;
+        float heading = m_shape.heading();
+        sf::Vector2f position = m_shape.position();
+
+        m_speed = control.outputs.body_speed;
+        heading += dt * m_speed * tanf(steering) / m_shape.dim.wheelbase;
+        position.x += dt * m_speed * cosf(heading);
+        position.y += dt * m_speed * sinf(heading);
+        m_shape.set(position, heading, steering);
+    }
+
+    virtual float acceleration() const override
+    {
+        return 0.0f;
+    }
+
+    virtual float speed() const override
+    {
+        return m_speed;
+    }
+
+    virtual sf::Vector2f position() const override
+    {
+        return m_shape.position();
     }
 
 private:
 
-    CarShape& shape;
+    CarShape& m_shape;
+    float m_speed = 0.0f;
+};
+
+class Trajectory
+{
+public:
+
+    Trajectory(CarDimension const& dim)
+        : m_dim(dim), m_min_turning_radius(dim, dim.max_steering_angle)
+    {}
+
+    // ---------------------------------------------------------------------
+    //! \brief Compute trajectory waypoints.
+    //! \param[in] Xi: the X world coordinate of the initial position of the
+    //! middle of the back wheel axle of the car at the begining of the
+    //! maneuver.
+    //! \param[in] Yi: the Y world coordinate of the initial position of the
+    //! middle of the back wheel axle of the car at the begining of the
+    //! maneuver.
+    //! \param[in] Xf: the desired X world coordinate of the final
+    //! destination in where the middle of the back wheel axle of the car
+    //! has to be at the end of the maneuver.
+    //! \param[in] Yf: the desired Y world coordinate of the final
+    //! destination in where the middle of the back wheel axle of the car
+    //! has to be at the end of the maneuver.
+    // ---------------------------------------------------------------------
+    bool computePathPlanning(float const /*Xi*/, float const /*Yi*/,
+                             float const /*Xf*/, float const /*Yf*/)
+    {
+        return false;
+    }
+
+    // ---------------------------------------------------------------------
+    //! \brief Generated tables of timed references for the auto-pilot to
+    //! follow the desired trajectory computed by computePathPlanning().
+    //! \param[in] vmax: maximal velocity [m/s] for the car.
+    //! \param[in] ades: desired acceleration [m/s^2] for the car.
+    // ---------------------------------------------------------------------
+    void generateReferenceTrajectory(float const /*vmax*/, float const /*ades*/)
+    {}
+
+    inline float Rmin() const { return m_min_turning_radius.middle_front; }
+    inline float Rimin() const { return m_min_turning_radius.internal; }
+    inline float Remin() const { return m_min_turning_radius.external; }
+    inline float Rmintild() const { return m_min_turning_radius.internal + m_dim.width / 2.0f; }
+
+private:
+
+    CarDimension const& m_dim;
+    TurningRadius m_min_turning_radius;
 };
 
 class Car
 {
 public:
 
-    Car(CarDimension const& dim)
-        : shape(dim), turning_radius(dim, dim.max_steering_angle)
+    Car(CarDimension const& dim_)
+        : dim(dim_), m_shape(dim_), m_physics(m_shape)
     {}
 
     Car(const char* model)
-        : Car(CarDimensions::get(name))
+        : Car(CarDimensions::get(model))
     {}
 
-    inline float Rmin() const { return min_radius.fake_front; }
-    inline float Rimin() const { return min_radius.internal; }
-    inline float Remin() const { return min_radius.external; }
-    inline float Rmintild() const { return min_radius.internal + dim.width / 2.0f; }
+    inline void init(sf::Vector2f const& position, float const speed,
+                     float const heading, float const steering)
+    {
+        m_physics.init(position, speed, heading, steering);
+    }
+
+    void update(float const /*dt*/)
+    {
+
+    }
+
+    inline float acceleration() const
+    {
+        return m_physics.acceleration();
+    }
+
+    inline float speed() const
+    {
+        return m_physics.speed();
+    }
+
+    inline sf::Vector2f position() const
+    {
+        return m_physics.position();
+    }
+
+    inline float heading() const
+    {
+        return m_shape.heading();
+    }
+
+    inline std::array<Wheel, 4> const& wheels() const
+    {
+        return m_shape.wheels();
+    }
+
+    inline Wheel const& wheel(Wheel::Type const i) const
+    {
+        return m_shape.wheel(i);
+    }
+
+    inline CarShape const& shape() const
+    {
+        return m_shape;
+    }
 
 public:
 
-    CarShape shape;
+    CarDimension const dim;
 
 private:
 
-    TurningRadius min;
-    Control control;
-    Physic physic;
+    CarShape m_shape;
+    CarKinematic m_physics;
+    CarControl m_control;
 };
 
 class CarDrawable : public sf::Drawable
 {
 public:
 
-    CarSDrawable(Car const& car_)
-        : car(car_)
+    void bind(Car const& car)
     {
-        CarDimensions const& dim = car.shape.dim;
+        m_car = &car;
+        CarDimension const& dim = car.dim;
 
-        body.setSize(sf::Vector2f(dim.length, dim.width));
-        body.setOrigin(dim.back_overhang, body.getSize().y / 2); // Origin on the middle of the rear wheels
-        body.setFillColor(sf::Color(165, 42, 42));
-        body.setOutlineThickness(ZOOM);
-        body.setOutlineColor(sf::Color::Blue);
+        m_body_shape.setSize(sf::Vector2f(dim.length, dim.width));
+        m_body_shape.setOrigin(dim.back_overhang, m_body_shape.getSize().y / 2); // Origin on the middle of the rear wheels
+        m_body_shape.setFillColor(sf::Color(165, 42, 42));
+        m_body_shape.setOutlineThickness(ZOOM);
+        m_body_shape.setOutlineColor(sf::Color::Blue);
 
-        wheel.setSize(sf::Vector2f(dim.wheel_radius * 2, dim.wheel_width));
-        wheel.setOrigin(wheel.getSize().x / 2, wheels[i].getSize().y / 2);
-        wheel.setFillColor(sf::Color::Black);
-        wheel.setOutlineThickness(ZOOM);
-        wheel.setOutlineColor(sf::Color::Yellow);
+        m_wheel_shape.setSize(sf::Vector2f(dim.wheel_radius * 2, dim.wheel_width));
+        m_wheel_shape.setOrigin(m_wheel_shape.getSize().x / 2, m_wheel_shape.getSize().y / 2);
+        m_wheel_shape.setFillColor(sf::Color::Black);
+        m_wheel_shape.setOutlineThickness(ZOOM);
+        m_wheel_shape.setOutlineColor(sf::Color::Yellow);
     }
 
 protected:
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override final
     {
-        target.draw(body, states);
+        if (m_car == nullptr)
+            return ;
+
+        target.draw(m_body_shape, states);
         for (int i = 0; i < 4; ++i)
         {
-            wheel.setPosition(shape.wheels[i].position);
-            wheel.setRotation(RAD2DEG(shape.yaw +  shape.wheels[i].steering));
-            target.draw(wheel, states);
+            CarShape const& shape = m_car->shape();
+            Wheel const& wheel = shape.wheel(static_cast<Wheel::Type>(i));
+
+            m_wheel_shape.setPosition(wheel.position);
+            m_wheel_shape.setRotation(RAD2DEG(shape.heading() + wheel.steering));
+            target.draw(m_wheel_shape, states);
         }
     }
 
-public:
-
-    Car& car;
-
 private:
 
+    Car const* m_car = nullptr;
     //! \brief The body drawn as a rectangle.
-    sf::RectangleShape body;
+    mutable sf::RectangleShape m_body_shape;
     //! \brief The front wheel drawn as a rectangle.
-    sf::RectangleShape wheel;
+    mutable sf::RectangleShape m_wheel_shape;
 };
 
 class Simulation : public sf::Drawable
@@ -314,14 +494,14 @@ public:
 
     void addedCar(Car& c)
     {
-        m_cars.push_back(CarDrawable(c));
+        m_cars.push_back(c);
     }
 
     void update(float const dt)
     {
-        for (auto const& it = m_cars)
+        for (auto& it: m_cars)
         {
-            it.update(dt;
+            it.update(dt);
         }
     }
 
@@ -329,13 +509,16 @@ protected:
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override final
     {
-        for (auto const& it = m_cars)
+        CarDrawable d;
+
+        for (auto const& it: m_cars)
         {
-            it.draw(target, states);
+            d.bind(it);
+            target.draw(d, states);
         }
     }
 
-    std::queu<CarDrawable> m_cars;
+    std::deque<Car> m_cars;
 };
 
 int main()
@@ -346,4 +529,3 @@ int main()
 
    return 0;
 }
-
