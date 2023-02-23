4f0f4c2e08f0ed0925559088ddbdecaef364ab64
Quentin Quadrat <lecrapouille@gmail.com>
2023-02-23 23:52:54 +0100

    Update Makefile

--- external/compile-external-libs.sh
+++ external/compile-external-libs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-
 ###############################################################################
 ### This script is called by (cd .. && make compile-external-libs). It will
 ### compile thirdparts cloned previously with make download-external-libs.
@@ -22,37 +21,8 @@
 ### find them when you'll start your application.
 ###############################################################################
 
-### $1 is given by ../Makefile and refers to the current architecture.
-if [ "$1" == "" ]; then
-  echo "Expected one argument. Select the architecture: Linux, Darwin or Windows"
-  exit 1
-fi
-
-ARCHI="$1"
-TARGET="$2"
-CC="$3"
-CXX="$4"
-
-function print-compile
-{
-    echo -e "\033[35m*** Compiling:\033[00m \033[36m$TARGET\033[00m <= \033[33m$1\033[00m"
-}
-
-### Number of CPU cores
-NPROC=
-if [[ "$ARCHI" == "Darwin" ]]; then
-    NPROC=`sysctl -n hw.logicalcpu`
-else
-    NPROC=`nproc`
-fi
+source ../.makefile/compile-external-libs.sh
 
 ### Basic logger for my GitHub C++ projects
 print-compile MyLogger
-if [ -e MyLogger ];
-then
-    (cd MyLogger
-     VERBOSE=1 make CXX=$CXX CC=$CC -j$NPROC
-    )
-else
-    echo "Failed compiling external/SOIL: directory does not exist"
-fi
+(cd MyLogger && call-make)
